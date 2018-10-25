ffb2fde8cd413cca8e82c2bfaccb15d3c39547c7
LockedOutOfGithub <np1249a@gmail.com>
2018-10-26 01:55:14 +0300

    memberimporter moved into class looking fresh

--- test/models/volunteer_test.rb
+++ test/models/volunteer_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
-class VolunteerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# class VolunteerTest < ActiveSupport::TestCase
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
