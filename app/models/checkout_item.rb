933ef96da7088a7646cbdd4f5c80feacccbd7f71
LockedOutOfGithub <np1249a@gmail.com>
2018-11-06 17:12:00 +0200

    forgot to commit build_default_checkout items

--- app/models/checkout_item.rb
+++ app/models/checkout_item.rb
@@ -1,7 +1,13 @@
 class CheckoutItem < ApplicationRecord
  	belongs_to :item
 
- 	def self.create_from_params(checkout_item_params)
- 	end
+	def self.build_default_items
+    [
+      ["Toiletries", "Soap"],
+      ["Toiletries", "Shampoo"]
+    ].map do |type_and_size|
+      item = Item.find_by(item_type: ItemType.find_by(name: type_and_size[0]), size: Size.find_by(name: type_and_size[1]))
+      CheckoutItem.new(item: item, count: 1)
+    end
 end
 
