3ffe08a8ce2a0cbe988783e90e808c1a86a196ac
LockedOutOfGithub <np1249a@gmail.com>
2018-11-28 12:30:11 +0200

    edit checkout

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -12,8 +12,39 @@ class CheckoutsController < ApplicationController
 		@items = Item.all
 	end
 
+	def edit
+		@checkout = Checkout.find(params[:id])
+		@appointment = @checkout.appointment
+		@items = Item.all
+	end
+
 	def create
 		@checkout = Checkout.new(checkout_params)
+		@checkout.checkout_items.select {|c_item| c_item.count.nil? }.map(&:destroy)
+
+		# check for duplicate items
+		if @checkout.checkout_items.uniq {|ci| ci.item_id }.length != @checkout.checkout_items.length
+			flash.now[:error] = "Failed to create checkout: item types must be unique"
+			@appointment = @checkout.appointment
+			@items = Item.all
+
+			render 'new'
+		else
+			if @checkout.save
+				@checkout.update_attribute(:status, 'completed')
+				@checkout.appointment.update_attribute(:attended, true)
+				flash.now[:success] = "Checkout completed"
+				render 'show'
+			else
+				render 'new', checkout: @checkout
+			end
+		end
+	end
+
+	def update
+		@checkout = Checkout.find(params[:id])
+		@checkout.update_attributes(checkout_params)
+
 		@checkout.checkout_items.select {|c_item| c_item.count.nil? }.map(&:destroy)
 
 		# check for duplicate items
@@ -43,6 +74,6 @@ class CheckoutsController < ApplicationController
 	private
 
 	def checkout_params
-		params.require(:checkout).permit(:appointment_id, checkout_items_attributes: [:_destroy, :item_id, :count])
+		params.require(:checkout).permit(:id, :appointment_id, checkout_items_attributes: [:id, :_destroy, :item_id, :count])
 	end
 end
